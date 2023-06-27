b7bdde18729f1dcff6916c5c5b2c8d74f75cb97e
mohamed-rafat2001 <mohamed20rafat@gmail.com>
2023-06-28 01:01:27 +0300

    update

--- server/routers/brandCategory.js
+++ server/routers/brandCategory.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const brandCat = require('../models/brandCategory')
 const auth = require('../middelwares/auth')
+const Product = require('../models/products')
 const route = express.Router()
 // add catagory
 route.post('/brandCat', auth.user, auth.admin, async (req, res) => {
@@ -25,10 +26,10 @@ route.patch('/brandCat/:id', auth.user, auth.admin, async (req, res) => {
     }
 })
 // get single category
-route.get('/brandCat/:id', auth.user, auth.admin, async (req, res) => {
+route.get('/brandCat/:cat', async (req, res) => {
     try {
-        const _id = req.params.id
-        const cat = await brandCat.findById(_id)
+        const Brand = req.params.cat
+        const cat = await Product.find({ Brand })
         if (!cat) return res.send('no category founded')
         res.send(cat)
     }
@@ -42,14 +43,24 @@ route.delete('/brandCat/:id', auth.user, auth.admin, async (req, res) => {
         const _id = req.params.id
         const cat = await brandCat.findByIdAndDelete(_id)
         if (!cat) return res.send('no category founded')
-        res.send({ cat, message: "Deleted successfully" })
+        res.send(cat)
     }
     catch (e) {
         res.send(e.message)
     }
 })
 // get all category
-route.get('/allBrandCat', auth.user, auth.admin, async (req, res) => {
+route.get('/admin/allBrandCat', auth.user, auth.admin, async (req, res) => {
+    try {
+        const cat = await brandCat.find()
+        if (!cat) return res.send('no category founded')
+        res.send(cat)
+    }
+    catch (e) {
+        res.send(e.message)
+    }
+})
+route.get('/allBrandCat', async (req, res) => {
     try {
         const cat = await brandCat.find()
         if (!cat) return res.send('no category founded')
